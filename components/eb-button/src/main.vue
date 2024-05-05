c0064fe1085b9cac4069c2e6f41776704151378d
Enbrau <admin@enbrau.com>
2024-05-05 20:34:13 +0800

    feat: add wave effect and disabled feature to EbButton

--- components/eb-button/src/main.vue
+++ components/eb-button/src/main.vue
@@ -1,5 +1,5 @@
 <template>
-  <div :class="classNames" @click.prevent="$emit('click')">
+  <div ref="btn" :class="classNames" @click.prevent="handleClick">
     <span>
       <slot></slot>
     </span>
@@ -43,6 +43,34 @@ export default {
         'is-round': this.round
       }
     }
+  },
+  methods: {
+    handleClick(event) {
+      if (this.disabled)  {
+        return
+      }
+
+      this.$emit('click')
+    
+      const btn = this.$refs.btn
+
+      const circle = document.createElement('span')
+      const diameter = Math.max(btn.clientWidth, btn.clientHeight)
+      const radius = diameter / 2
+
+      circle.style.width = circle.style.height = `${diameter}px`
+      circle.style.left  = `${event.clientX - btn.offsetLeft - radius}px`
+      circle.style.right = `${event.clientY - btn.offsetTop - radius}px`
+      circle.classList.add('wave')
+
+      const oldWave = btn.getElementsByClassName('wave')[0]
+
+      if (oldWave) {
+        oldWave.remove()
+      }
+
+      btn.appendChild(circle)
+    }
   }
 }
 </script>
@@ -51,7 +79,9 @@ export default {
 $colors: primary, success, danger, warning, info;
 
 .eb-button {
+  position: relative;
   display: inline-flex;
+  overflow: hidden;
   height: 36px;
   padding: 0 15px;
   align-items: center;
@@ -67,6 +97,10 @@ $colors: primary, success, danger, warning, info;
     border-radius: 18px;
   }
 
+  &.is-disabled {
+    cursor: not-allowed;
+  }
+
   @each $color in $colors {
     &.eb-button--#{$color} {
       color: #fff;
@@ -78,12 +112,23 @@ $colors: primary, success, danger, warning, info;
         border-color: var(--eb-color-#{$color}-light5);
       }
 
+      &.is-disabled, &.is-disabled:hover {
+        background-color:  var(--eb-color-#{$color}-light7);
+        border-color: var(--eb-color-#{$color}-light7);
+      }
+
       &.is-plain {
         color: var(--eb-color-#{$color});
         background-color: transparent;
 
         &:hover {
           background-color: var(--eb-color-#{$color}-light9);
+        }
+
+        &.is-disabled, &.is-disabled:hover {
+          background-color: transparent;
+          color: var(--eb-color-#{$color}-light7);
+          border-color: var(--eb-color-#{$color}-light7);
         }
       }
 
@@ -96,6 +141,25 @@ $colors: primary, success, danger, warning, info;
           background-color: var(--eb-color-info-light9);
           border-color: var(--eb-color-info-light9);
         }
+
+        &.is-disabled, &.is-disabled:hover {
+          background-color: transparent;
+          border-color: transparent;
+          color: var(--eb-color-#{$color}-light7);
+        }
+
+        .wave {
+          background-color: var(--eb-color-info);
+        }
+      }
+
+      .wave {
+        position: absolute;
+        border-radius: 50%;
+        transform: scale(0);
+        animation: wave 600ms linear;
+        background-color: var(--eb-color-#{$color});
+        opacity: .7;
       }
     }
   }
@@ -103,5 +167,12 @@ $colors: primary, success, danger, warning, info;
 
 .eb-button+.eb-button {
   margin-left: 10px;
+}
+
+@keyframes wave {
+  to {
+    transform: scale(4);
+    opacity: 0;
+  }
 }
 </style>
--- examples/src/App.vue
+++ examples/src/App.vue
@@ -2,28 +2,64 @@
   <eb-row :gutter="15">
     <eb-col :span="12" sm="24">
       <eb-example title="EbButton">
-        <eb-button v-for="type of btnTypes" :key="type" :type="type">
-          {{ $format.toCamelCase(type, true) }}
-        </eb-button>
+        <eb-row :gutter="20">
+          <eb-col :span="24">
+            <eb-button v-for="type of btnTypes" :key="type" :type="type">
+              {{ $format.toCamelCase(type, true) }}
+            </eb-button>
+          </eb-col>
+          <eb-col :span="24" style="margin-top: 10px;">
+            <eb-button v-for="type of btnTypes" :key="type" :type="type" disabled>
+              {{ $format.toCamelCase(type, true) }}
+            </eb-button>
+          </eb-col>
+        </eb-row>
       </eb-example>
     </eb-col>
     <eb-col :span="12" sm="24">
       <eb-example title="EbButton - plain">
-        <eb-button v-for="type of btnTypes" :key="type" :type="type" plain>
-          {{ $format.toCamelCase(type, true) }}
-        </eb-button>
+        <eb-row :gutter="20">
+          <eb-col :span="24">
+            <eb-button v-for="type of btnTypes" :key="type" :type="type" plain>
+              {{ $format.toCamelCase(type, true) }}
+            </eb-button>
+          </eb-col>
+          <eb-col :span="24" style="margin-top: 10px;">
+            <eb-button v-for="type of btnTypes" :key="type" :type="type" plain disabled>
+              {{ $format.toCamelCase(type, true) }}
+            </eb-button>
+          </eb-col>
+        </eb-row>
       </eb-example>
     </eb-col>
   </eb-row>
   <eb-example title="EbButton - text">
-    <eb-button v-for="type of btnTypes" :key="type" :type="type" text>
-      {{ $format.toCamelCase(type, true) }}
-    </eb-button>
+    <eb-row :gutter="20">
+      <eb-col :span="24">
+        <eb-button v-for="type of btnTypes" :key="type" :type="type" text>
+          {{ $format.toCamelCase(type, true) }}
+        </eb-button>
+      </eb-col>
+      <eb-col :span="24" style="margin-top: 10px;">
+        <eb-button v-for="type of btnTypes" :key="type" :type="type" text disabled>
+          {{ $format.toCamelCase(type, true) }}
+        </eb-button>
+      </eb-col>
+    </eb-row>
   </eb-example>
   <eb-example title="EbButton - round">
-    <eb-button v-for="type of btnTypes" :key="type" :type="type" round>
-      {{ $format.toCamelCase(type, true) }}
-    </eb-button>
+    <eb-row :gutter="20">
+      <eb-col :span="24">
+        <eb-button v-for="type of btnTypes" :key="type" :type="type" round>
+          {{ $format.toCamelCase(type, true) }}
+        </eb-button>
+      </eb-col>
+      <eb-col :span="24" style="margin-top: 10px;">
+        <eb-button v-for="type of btnTypes" :key="type" :type="type" round disabled>
+          {{ $format.toCamelCase(type, true) }}
+        </eb-button>
+      </eb-col>
+    </eb-row>
   </eb-example>
 </template>
 
