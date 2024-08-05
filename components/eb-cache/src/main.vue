2072c245a0c0e6c36ebbb3b81053d7cba29df45f
Enbrau <enbrau@gmail.com>
2024-08-05 09:09:58 +0800

    feat add cache component

---
+++ components/eb-cache/src/main.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { useCacheStore } from '../stores/cache'
+import { defineComputed, defineProps, expose } from 'vue'
+
+const cacheItem = defineComputed({
+  get: () => {
+    return useCacheStore().cacheItem
+  },
+  set: (value) => {
+    useCacheStore().cacheItem = value
+  }
+})
+
+</script>
